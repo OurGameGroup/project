422023df4decdb6c7727568d005768422dab4c86
Limiting <i405203@gmail.com>
2015-05-26 15:32:07 +0800

    show numbers when the tower get hurt

--- angrycastle/scripts/app/object/Castle.lua
+++ angrycastle/scripts/app/object/Castle.lua
@@ -15,9 +15,10 @@ end
 
 function Castle:damage()
 	self.hp = self.hp - 1
-
+	self:showDamage()
 	if(self.hp < 0)then
 		print "GameOver"
+
 	end
 end
 
@@ -27,6 +28,36 @@ end
 
 function Castle:underTower(point)
 	return self:getBoundingBox():containsPoint(point)
+end
+
+function Castle:showDamage()
+	local labelTTF = ui.newTTFLabelWithOutline({
+		text  = "-1",
+		size  = 60,
+		color = ccc3(255, 0, 0),
+		align = ui.TEXT_ALIGN_RIGHT,
+		x     = self:getTowerTop().x,
+		y     = self:getTowerTop().y,
+		outlineColor = ccc3(255, 255, 0)
+		})
+	self:addChild(labelTTF)
+
+	local move1 = CCMoveBy:create(1, CCPoint(50, 50))
+   	local move2 = CCFadeOut:create(1)
+
+   	local ccarray = CCArray:create()
+   	ccarray:addObject(move1)
+   	ccarray:addObject(move2)
+   	
+   	local SpawnAction = CCSpawn:create(ccarray)
+   	-- labelTTF:playAnimationOnce(SpawnAction,true)
+
+   	-- labelTTF:runAction(SpawnAction)
+   	transition.execute(labelTTF, SpawnAction, {
+   		onComplete = function ()
+   			self:removeChild(labelTTF)
+   		end
+   	})
 end
 
 return Castle
