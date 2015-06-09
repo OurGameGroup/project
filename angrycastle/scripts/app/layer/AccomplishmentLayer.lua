965266be02b36ef3e566a4094991b8f91f4672bb
Limiting <i405203@gmail.com>
2015-06-09 10:56:21 +0800

    fix some bug

--- angrycastle/scripts/app/layer/AccomplishmentLayer.lua
+++ angrycastle/scripts/app/layer/AccomplishmentLayer.lua
@@ -12,24 +12,26 @@ function AccomplishmentLayer:init()
 end
 
 function AccomplishmentLayer:showAcmpmt(name)
-	self.img = display.newSprite(name)
-	self.img:setAnchorPoint(ccp(0,0))
-	self.img:setPosition(display.width / 3, display.height * 3 / 5)
-	self.img:setScale(0.3)
-	self:addChild(self.img)
+	local img = display.newSprite(name)
+	img:setAnchorPoint(ccp(0,0))
+	img:setPosition(display.width / 3, display.height * 3 / 5)
+	img:setScale(0.3)
+	self:addChild(img)
 
 
 	local action1 = CCScaleBy:create(0.5,1.7)
-	local action2 = CCFadeOut:create(1)
+	local action2 = CCScaleBy:create(2,1)
+	local action3 = CCFadeOut:create(1)
 	local ccarray = CCArray:create()
    	ccarray:addObject(action1)
    	ccarray:addObject(action2)
+   	ccarray:addObject(action3)
 
    	local SequenceAction = CCSequence:create(ccarray)
 
-	transition.execute(self.img, SequenceAction, {
+	transition.execute(img, SequenceAction, {
    		onComplete = function ()
-   			self:removeChild(self.img)
+   			self:removeChild(img)
    		end
    	})
 end
